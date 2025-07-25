38e3d5da5716799172f21e70b09c4fb14e91d574
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-25 18:05:12 +0400

    Add auto-bootstrap functionality to Container
    Eliminates the need for users to manually require bootstrap/container.php
    when using the Client class. Container now automatically initializes on
    first access with lazy loading support.

--- src/Container.php
+++ src/Container.php
@@ -2,14 +2,21 @@
 
 namespace Blugen;
 
+use Blugen\Config\ConfigManager;
+use Composer\Autoload\ClassLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 /**
- * Simple container accessor class
+ * Container accessor with auto-bootstrap capability
  */
 class Container
 {
     private static ?ContainerInterface $container = null;
+    private static bool $autoBootstrapAttempted = false;
 
     public static function set(ContainerInterface $container): void
     {
@@ -18,10 +25,67 @@ class Container
 
     public static function get(): ContainerInterface
     {
+        if (null === self::$container && !self::$autoBootstrapAttempted) {
+            self::autoBootstrap();
+        }
+
         if (null === self::$container) {
-            throw new \LogicException('Container has not been set yet.');
+            throw new \LogicException('Container could not be initialized automatically.');
         }
 
         return self::$container;
+    }
+
+    private static function autoBootstrap(): void
+    {
+        self::$autoBootstrapAttempted = true;
+
+        try {
+            // Find the appropriate autoloader
+            $possibleAutoloaders = [
+                __DIR__ . '/../../../../autoload.php',              // When installed in vendor/shahmal1yev/blugen
+                __DIR__ . '/../vendor/autoload.php',               // When using the package directly
+                __DIR__ . '/../../../vendor/autoload.php',         // Alternative location
+            ];
+
+            $autoloader = null;
+            foreach ($possibleAutoloaders as $file) {
+                if (file_exists($file)) {
+                    $autoloader = $file;
+                    break;
+                }
+            }
+
+            if ($autoloader === null) {
+                return; // Fail silently, let the LogicException be thrown
+            }
+
+            /** @var ClassLoader $classLoader */
+            $classLoader = require $autoloader;
+
+            // Load configurations
+            $configManager = ConfigManager::load();
+
+            // Set up the container
+            $container = new ContainerBuilder();
+            $container->set('loader', $classLoader);
+            $container->set(ConfigManager::class, $configManager);
+
+            // Add config as container parameter
+            $container->setParameter('blugen.config', $configManager->all());
+
+            // Load container services
+            $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+            $loader->load('services.php');
+
+            // Compile the container
+            $container->compile();
+
+            // Set the container
+            self::$container = $container;
+        } catch (\Throwable $e) {
+            // Fail silently, let the original LogicException be thrown
+            return;
+        }
     }
 }
