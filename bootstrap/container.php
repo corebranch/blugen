afac77e029a27dedbdf8798a77970844c6147fe2
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-25 16:18:55 +0400

    Update autoloader priority and add bin configuration
    - Prioritize vendor autoloader for proper package installation
    - Add bin configuration to composer.json for CLI tool registration

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -10,8 +10,8 @@ use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 return (function () {
     // Find the appropriate autoloader
     $possibleAutoloaders = [
-        __DIR__ . '/../vendor/autoload.php',             // When using the package directly
         __DIR__ . '/../../../autoload.php',              // When installed in vendor/shahmal1yev/blugen
+        __DIR__ . '/../vendor/autoload.php',             // When using the package directly
         __DIR__ . '/../../../vendor/autoload.php',       // Alternative location
     ];
 
