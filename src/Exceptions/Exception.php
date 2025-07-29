f35d56f0f6455de4bc0041d1ac20d0f4d8b52235
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-07-29 13:09:43 +0400

    Enhance exception classes with context and error codes
    - Added context support to base `Exception` class with `getContext()` and `withContext()` methods
    - Implemented unique error codes for each exception type (1001-1003)
    - Enhanced `PrefixNotDefined` with helpful default message referencing config/codegen.php
    - Updated `PrefixNotFound` to include namespace context and composer dump-autoload guidance
    - Improved `PrefixPathNotDirectory` with path context and actionable error messages

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -4,4 +4,22 @@ namespace Blugen\Exceptions;
 
 abstract class Exception extends \Exception
 {
+    protected array $context = [];
+    
+    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null, array $context = [])
+    {
+        parent::__construct($message, $code, $previous);
+        $this->context = $context;
+    }
+    
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+    
+    public function withContext(array $context): self
+    {
+        $this->context = array_merge($this->context, $context);
+        return $this;
+    }
 }
