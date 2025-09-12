5989ace16475e62962c27914060ca02e85ba84c2
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-09-12 16:02:46 +0400

    Stop suppressing exceptions during container autobootstrap

--- src/Container.php
+++ src/Container.php
@@ -40,61 +40,56 @@ class Container
     {
         self::$autoBootstrapAttempted = true;
 
-        try {
-            // Get the already loaded ClassLoader from Composer
-            $classLoader = null;
-            
-            // Try to get ClassLoader from registered autoloaders
-            foreach (spl_autoload_functions() as $autoloader) {
-                if (is_array($autoloader) && $autoloader[0] instanceof ClassLoader) {
-                    $classLoader = $autoloader[0];
+        // Get the already loaded ClassLoader from Composer
+        $classLoader = null;
+
+        // Try to get ClassLoader from registered autoloaders
+        foreach (spl_autoload_functions() as $autoloader) {
+            if (is_array($autoloader) && $autoloader[0] instanceof ClassLoader) {
+                $classLoader = $autoloader[0];
+                break;
+            }
+        }
+
+        // Fallback: try to find and require autoloader if not found
+        if (!$classLoader) {
+            $possibleAutoloaders = [
+                __DIR__ . '/../../../../autoload.php',              // When installed in vendor/shahmal1yev/blugen
+                __DIR__ . '/../vendor/autoload.php',               // When using the package directly
+                __DIR__ . '/../../../vendor/autoload.php',         // Alternative location
+            ];
+
+            foreach ($possibleAutoloaders as $file) {
+                if (file_exists($file)) {
+                    $classLoader = require $file;
                     break;
                 }
             }
-
-            // Fallback: try to find and require autoloader if not found
-            if (!$classLoader) {
-                $possibleAutoloaders = [
-                    __DIR__ . '/../../../../autoload.php',              // When installed in vendor/shahmal1yev/blugen
-                    __DIR__ . '/../vendor/autoload.php',               // When using the package directly
-                    __DIR__ . '/../../../vendor/autoload.php',         // Alternative location
-                ];
-
-                foreach ($possibleAutoloaders as $file) {
-                    if (file_exists($file)) {
-                        $classLoader = require $file;
-                        break;
-                    }
-                }
-            }
-
-            if (!$classLoader) {
-                return; // Fail silently
-            }
-
-            // Load configurations
-            $configManager = ConfigManager::load();
-
-            // Set up the container
-            $container = new ContainerBuilder();
-            $container->set('loader', $classLoader);
-            $container->set(ConfigManager::class, $configManager);
-
-            // Add config as container parameter
-            $container->setParameter('blugen.config', $configManager->all());
-
-            // Load container services
-            $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../config'));
-            $loader->load('services.php');
-
-            // Compile the container
-            $container->compile();
-
-            // Set the container
-            self::$container = $container;
-        } catch (\Throwable $e) {
-            // Fail silently to avoid breaking existing installations
-            return;
         }
+
+        if (!$classLoader) {
+            throw new \RuntimeException('Autoloader not found.');
+        }
+
+        // Load configurations
+        $configManager = ConfigManager::load();
+
+        // Set up the container
+        $container = new ContainerBuilder();
+        $container->set('loader', $classLoader);
+        $container->set(ConfigManager::class, $configManager);
+
+        // Add config as container parameter
+        $container->setParameter('blugen.config', $configManager->all());
+
+        // Load container services
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+        $loader->load('services.php');
+
+        // Compile the container
+        $container->compile();
+
+        // Set the container
+        self::$container = $container;
     }
 }
